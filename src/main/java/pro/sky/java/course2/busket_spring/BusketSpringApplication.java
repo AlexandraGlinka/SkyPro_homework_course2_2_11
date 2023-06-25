ac30621f02c92f45a3d170bae135b8a75f700b32
glinka <alexandraglinka@yandex.ru>
2023-06-25 16:59:49 +0300

    initial commit

---
+++ src/main/java/pro/sky/java/course2/busket_spring/BusketSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.busket_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusketSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/busket_spring/BusketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.busket_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusketSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
